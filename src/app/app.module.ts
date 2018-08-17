83931d110b4f2c786b0eaef7096e21bf43f1d3a3
Nikki Boyd <boyd.nikki@icloud.com>
2018-08-17 14:00:02 -0700

    begin styling board header and lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 import { ListsComponent } from './lists/lists.component';
