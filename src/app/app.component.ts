08dff61318b0d8f1e55577b4671b6692c6394b04
Nikki Boyd <boyd.nikki@icloud.com>
2018-08-17 16:38:31 -0700

    final commit, upload screenshot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,8 @@ import { Board } from './models/board.model';
 export class AppComponent {
   allLists: List[] = [
     new List('Current Projects', [new Card('Trello Clone'), new Card('Recipe Management App'), new Card('Brainstorm Group Project Ideas')]),
-    new List('Completed Projects', [new Card('DocGawk API'), new Card('ICE Raid Tracker'), new Card('Island Pie Pizza Website'), new Card('Keg Inventory System')])
+    new List('Completed Projects', [new Card('DocGawk API'), new Card('ICE Raid Tracker'), new Card('Island Pie Pizza Website'), new Card('Keg Inventory System')]),
+    new List('Project Ideas', [new Card('Professional Portfolio'), new Card('Personal Blog/Website'), new Card('Grocery Budgeting Tool')]),
   ];
 
   selectedList = null;
